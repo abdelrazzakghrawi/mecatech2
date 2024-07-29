437bee16a96d729e955fb6fd9079744eb6dbf9b2
hatim mordy <hmordy02@gmail.com>
2024-07-29 10:09:21 +0000

    added docker file to search service and the font end application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PickCarContainer from './searchService/searchComponent/PickCarContainer';
-import CitySearchContainer from './searchService/searchComponent/CitySearchContainer';
-import PresationContainer from './searchService/searchComponent/PresationContainer';
 import SearchBar from './searchService/searchComponent/SearchBar';
 function App() {
 
