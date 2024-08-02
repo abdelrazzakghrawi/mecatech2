a4f2da4006c5653006338e50a5b087550a4a6ffb
addiayoub <addiayoub2005@gmail.com>
2024-08-02 10:19:29 +0100

    Votre message de commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Navbar from "./LandingPage/Navbar/Navbar"
+import Slider from "./LandingPage/slider/Slider"
+import Hover from "./LandingPage/icons/Hover"
+import LESAVIS from "./LandingPage/Avis client/LESAVIS"
+import Footer from "./LandingPage/Footer/Footer"
+
+
+
+const App = () => {
+  return (
+    <div>
+     
+<Navbar/>
+<Slider/>
+<Hover/>
+<LESAVIS/>
+<Footer/>
+    </div>
+    
+  )
+}
+
+export default App
