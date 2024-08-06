3a89639b5b9d40088ea240a3612e6a4fdffadaa8
hatim mordy <hmordy02@gmail.com>
2024-08-06 14:04:04 +0000

    just some css search botton

--- client/src/searchService/searchComponent/SearchButton.jsx
+++ client/src/searchService/searchComponent/SearchButton.jsx
@@ -6,7 +6,7 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 const SearchButton = ({ onClick }) => (
   <div className='flex-shrink-0'>
     <button
-      className="bg-[#1FA9B6] text-white px-8 py-6 hover:bg-[#148a94] flex items-center mt-4 md:mt-0 rounded-md transition duration-300 ease-in-out transform hover:scale-105"
+      className="bg-[#1FA9B6] text-white px-8 py-6 hover:bg-[#148a94] flex items-center mt-4 md:mt-0  transition duration-300 ease-in-out transform "
       onClick={onClick}
     >
       Chercher mecanique
