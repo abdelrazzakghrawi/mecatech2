961f949d13dbc6dc2be7bd166c21c695abfaa795
hatim mordy <hmordy02@gmail.com>
2024-08-23 10:13:30 +0000

    some conf for rendering search service

--- server/services/search-service/src/index.js
+++ server/services/search-service/src/index.js
@@ -4,16 +4,13 @@ const connectDB = require('./config/db');
 const cors = require("cors");
 const path = require('path');
 
-
-
-
+const app = express();
 
 const corsOptions = {
-  origin: 'https://frontend-57c4.onrender.com'
-  
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  optionsSuccessStatus: 200
 };
 
-const app = express();
 app.use(cors(corsOptions));
 
 // Connect to MongoDB
@@ -21,6 +18,7 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+
 // Serve static files
 app.use('/garage_images', express.static(path.join(__dirname, 'garage_images')));
 app.use('/meca_images', express.static(path.join(__dirname, 'random_image_meca')));
@@ -29,7 +27,7 @@ app.use('/meca_images', express.static(path.join(__dirname, 'random_image_meca')
 app.use('/api/auto', require('./routes/autoRoutes'));
 app.use('/api/search', require('./routes/searchRoutes'));
 app.use('/api/prestations', require('./routes/prestationsRoutes')); 
-app.use('/api/villequartier', require('./routes/villeQuartierRoutes')); // Registering the new routes
+app.use('/api/villequartier', require('./routes/villeQuartierRoutes'));
 
 // Start server
 const PORT = process.env.PORT || 3003;
