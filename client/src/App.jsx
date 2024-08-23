4abf5c0d17216f03c3c6218f38ed1cd659914a1d
abderrazzak ghrawi <a.razzak2002@gmail.com>
2024-08-23 13:47:30 +0100

    Fix deployment configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import VerifyEmail from './Auth/VerifyEmail/VerifyEmail';
 import Animation from "../src/Animation/Animation"
 
 import './App.css'
+import axios from 'axios';
+import { useEffect } from 'react';
 
 Modal.setAppElement('#root');
 
@@ -17,10 +19,25 @@ const App = () => {
   const { role } = useAuth();
   console.log('Current role:', role);
 
+  // Envoi de la requête POST lors du montage du composant
+  useEffect(() => {
+    const postData = async () => {
+      try {
+        const response = await axios.post('http://deploy-mern-stack.vercel.app', {
+          // Données à envoyer avec la requête POST
+          role: role,
+        });
+        console.log('Réponse de la requête POST:', response.data);
+      } catch (error) {
+        console.error('Erreur lors de la requête POST:', error);
+      }
+    };
+
+    postData();
+  }, [role]); // La requête POST sera envoyée chaque fois que le rôle change
+
   return (
-   
     <Router>
-      
       <div>
         <Animation/>
         <Routes>
@@ -28,16 +45,13 @@ const App = () => {
           {role === 'mecano' && (
             <Route path="/dashboard-mecano" element={<Espacemeca/>} />
           )}
-                      <Route path="/dashboard-mecano" element={<Espacemeca/>} />
-
+          <Route path="/dashboard-mecano" element={<Espacemeca/>} />
           {role === 'client' && (
             <Route path="/dashboard-client" element={<DashboardClient />} />
           )}
-           <Route path="/verify-email/:token" element={<VerifyEmail />} />
+          <Route path="/verify-email/:token" element={<VerifyEmail />} />
         </Routes>
-        
       </div>
-    
     </Router>
   );
 };
